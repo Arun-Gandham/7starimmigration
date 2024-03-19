b2d5151170242699981c8f41a0f43d54f82bf5dd
arunsai.gandham <arunsai.gandham@karkinos.in>
2024-03-19 10:31:35 +0530

    added logo

--- resources/views/templates/auth/login.blade.php
+++ resources/views/templates/auth/login.blade.php
@@ -35,8 +35,7 @@
                 <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
                     <img src="{{ asset('assets/img/illustrations/auth-login-illustration-' . $configData['style'] . '.png') }}"
                         alt="auth-login-cover" class="img-fluid my-5 auth-illustration"
-                        data-app-light-img="illustrations/auth-login-illustration-light.png"
-                        data-app-dark-img="illustrations/auth-login-illustration-dark.png">
+                        data-app-light-img="illustrations/logo.png" data-app-dark-img="illustrations/logo.png">
 
                     <img src="{{ asset('assets/img/illustrations/bg-shape-image-' . $configData['style'] . '.png') }}"
                         alt="auth-login-cover" class="platform-bg"
